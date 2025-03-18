2ed573e41b42846c7744bb53febf4e2456761139
人間工作 <coolxiaom95@gmail.com>
2025-03-19 00:43:12 +0800

    create site build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf output
+mkdir output
+cp -r www/* output
+npm install uglify-js uglifycss -g
+uglifyjs www/js/utatap.js -o output/js/utatap.js
+uglifycss www/css/utatap.css > output/css/utatap.css
+cp -r data output
+rm output/data/README.md
+ls -N1U output/data/vocal > output/data/vocal.txt
+ls -N1U output/data/music > output/data/music.txt
