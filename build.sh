fb09684827a25085729e0d7dd9f06fe0921fc4a4
MrXiaoM <coolxiaom95@gmail.com>
2025-09-22 18:07:59 +0800

    prevent overwrite css with empty content

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-rm -rf output
-mkdir output
-cp -r www/* output
+rm -rf ./output
+mkdir ./output
+mkdir ./output/js
+mkdir ./output/css
+cp -r ./www/* ./output
 npm install uglify-js uglifycss -g
-uglifyjs www/js/utatap.js -o output/js/utatap.js
-uglifycss www/css/utatap.css > output/css/utatap.css
-cp -r data output
-rm output/data/README.md
-ls -N1U output/data/vocal > output/data/vocal.txt
-ls -N1U output/data/music > output/data/music.txt
+uglifyjs ./www/js/utatap.js -o ./output/js/utatap.js
+uglifycss ./www/css/utatap.css --output ./output/css/utatap.css
+cp -r ./data ./output
+rm ./output/data/README.md
+ls -N1U ./output/data/vocal > ./output/data/vocal.txt
+ls -N1U ./output/data/music > ./output/data/music.txt
