691fe364c607596a667a4877442ec14daf564bf4
MrXiaoM <coolxiaom95@gmail.com>
2025-09-28 01:21:13 +0800

    add track mute icon

--- editor/Page/PageEditTracks.xaml.cs
+++ editor/Page/PageEditTracks.xaml.cs
@@ -5,19 +5,35 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace editor
 {
     public class MusicTrack
     {
+        private static Path IconToMute => new()
+        {
+            Data = Geometry.Parse("M333.1 34.8C344.6 40 352 51.4 352 64l0 384c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L163.8 352 96 352c-35.3 0-64-28.7-64-64l0-64c0-35.3 28.7-64 64-64l67.8 0L298.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zm172 72.2c43.2 35.2 70.9 88.9 70.9 149s-27.7 113.8-70.9 149c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C507.3 341.3 528 301.1 528 256s-20.7-85.3-53.2-111.8c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zm-60.5 74.5C466.1 199.1 480 225.9 480 256s-13.9 56.9-35.4 74.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C425.1 284.4 432 271 432 256s-6.9-28.4-17.7-37.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5z"),
+            Fill = Brushes.Black,
+            Stretch = Stretch.Uniform,
+            Margin = new Thickness(1),
+        };
+        private static Path IconToUnMute => new()
+        {
+            Data = Geometry.Parse("M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.1 386.2C556.7 352 576 306.3 576 256c0-60.1-27.7-113.8-70.9-149c-10.3-8.4-25.4-6.8-33.8 3.5s-6.8 25.4 3.5 33.8C507.3 170.7 528 210.9 528 256c0 39.1-15.6 74.5-40.9 100.5L449 326.6c19-17.5 31-42.7 31-70.6c0-30.1-13.9-56.9-35.4-74.5c-10.3-8.4-25.4-6.8-33.8 3.5s-6.8 25.4 3.5 33.8C425.1 227.6 432 241 432 256s-6.9 28.4-17.7 37.3c-1.3 1-2.4 2.2-3.4 3.4L352 250.6 352 64c0-12.6-7.4-24-18.9-29.2s-25-3.1-34.4 5.3L197.8 129.8 38.8 5.1zM352 373.3L82.9 161.3C53.8 167.4 32 193.1 32 224l0 64c0 35.3 28.7 64 64 64l67.8 0L298.7 471.9c9.4 8.4 22.9 10.4 34.4 5.3S352 460.6 352 448l0-74.7z"),
+            Fill = Brushes.Black,
+            Stretch = Stretch.Uniform,
+        };
         PageEditTracks parent;
         Action<MusicTrack, int> onSelected;
         public Grid left;
         public StackPanel right;
+        private TextBlock buttonMute;
         public string comment;
         public bool loop;
         public List<int> notes;
+        public bool mute = false;
         internal List<Grid> noteGrids = new List<Grid>();
         internal int selectedIndex = -1;
         private static Brush hex(string s)
@@ -35,18 +51,36 @@ namespace editor
             {
                 Height = 24,
             };
+            this.left.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
             this.left.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
             this.left.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
+            buttonMute = new TextBlock()
+            {
+                Width = 18, Height = 14,
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                Margin = new Thickness(4, 2, 0, 2),
+                Cursor = Cursors.Hand,
+            };
+            buttonMute.MouseLeftButtonDown += (s, e) =>
+            {
+                mute = !mute;
+                UpdateMuteIcon();
+                e.Handled = true;
+            };
             var textTrackName = new TextBlock()
             {
                 Text = comment,
                 VerticalAlignment = VerticalAlignment.Center,
                 HorizontalAlignment = HorizontalAlignment.Left,
-                Margin = new Thickness(4, 2, 0, 2),
+                Margin = new Thickness(2, 2, 0, 2),
             };
-            Grid.SetColumn(textTrackName, 0);
+            Grid.SetColumn(buttonMute, 0);
+            Grid.SetColumn(textTrackName, 1);
+            this.left.Children.Add(buttonMute);
             this.left.Children.Add(textTrackName);
-            this.left.Children.Add(GenerateCheckBox());
+            this.left.Children.Add(GenerateCheckBox(2));
+            UpdateMuteIcon();
             SetupLeftContextMenu(this.left, textTrackName);
             this.right = new StackPanel()
             {
@@ -63,6 +97,13 @@ namespace editor
 
             left.Children.Insert(index, this.left);
             right.Children.Insert(index, this.right);
+        }
+
+        public void UpdateMuteIcon()
+        {
+            buttonMute.Inlines.Clear();
+            buttonMute.Inlines.Add(mute ? IconToUnMute : IconToMute);
+            buttonMute.ToolTip = mute ? "取消静音" : "静音";
         }
 
         public void UpdateBackgroundByIndex(int index)
@@ -102,7 +143,7 @@ namespace editor
             grid.ContextMenu = ctx;
         }
 
-        private CheckBox GenerateCheckBox()
+        private CheckBox GenerateCheckBox(int column)
         {
             var check = new CheckBox()
             {
@@ -112,7 +153,7 @@ namespace editor
                 Margin = new Thickness(0, 0, 4, 0),
                 IsChecked = loop,
             };
-            Grid.SetColumn(check, 1);
+            Grid.SetColumn(check, column);
             check.Checked += (o, e) =>
             {
                 if (!this.loop)
@@ -439,8 +480,10 @@ namespace editor
 
         public void PlayPreviewFrame(int index)
         {
-            foreach (var track in previewTracks)
+            for (int i = 0; i < previewTracks.Count; i++)
             {
+                var track = previewTracks[i];
+                if (tracks[i].mute) continue;
                 int note = track.notes[index];
                 float volume = track.volumes[index];
                 if (note == -1) continue;
